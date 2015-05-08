4fe7068a518e80bd310be28f725a09447700610e
Chris Peel <chris.peel@ieee.org>
2015-05-07 17:44:19 -0700

    Save png figures in right directory

--- test/runtests.jl
+++ test/runtests.jl
@@ -6,7 +6,7 @@ using LDPCsim
 
 
 ldpcSim(1000,[-2.0:1:10],[50])
-savefig("test/ldpcVsSNR_M50rHalf.png") # run from root directory
+savefig("ldpcVsSNR_M50rHalf.png") # run from root directory
 
 ldpcSim(1000,[3.5],[4,8,16,32,64,128])
-savefig("test/ldpcVsM_snr3p5_rHalf.png") # run from root directory
+savefig("ldpcVsM_snr3p5_rHalf.png") # run from root directory
